2afee99bc60e762bb588d57d18166ef87f14d4c6
Skyler Brungardt <skyler.brungardt@gmail.com>
2018-07-20 12:34:57 -0700

    Using Harbormaster to load pkgs

--- ssh.js
+++ ssh.js
@@ -1,44 +1,38 @@
-
-const dependencies = [
-  'ssh2', 'expand-tilde', 'js-htmlencode', 'lodash'
-].join(' ');
-
-console.log('Installing dependencies:', dependencies, '...');
-require('child_process').execSync(
-  'npm i ' + dependencies
-);
-console.log('Dependencies installed:', dependencies);
-
-const fs = require('fs');
-const path = require('path');
-const expandTilde = require('expand-tilde');
-const harbor_dir = process.env.HARBORMASTER_HARBORS_DIR ||
-  process.env.HARBORMASTER_SSH_DIR ||
-  expandTilde('~/.harbormaster/harbors');
-
+const name = 'ssh';
+const pkgs = [
+  'ssh2', 'expand-tilde', 'js-htmlencode', 'lodash',
+];
+let fs;
+let path;
+let expandTilde;
+let harbor_dir;
 let Shipments;
-const NAME = 'ssh';
 
 module.exports = {
+  next: () => {
+    fs = require('fs');
+    path = require('path');
+    expandTilde = require('expand-tilde');
+    harbor_dir = process.env.HARBORMASTER_HARBORS_DIR ||
+      process.env.HARBORMASTER_SSH_DIR ||
+      expandTilde('~/.harbormaster/harbors');
 
-  render_input: eval(
-    fs.readFileSync(harbor_dir + '/ssh/render_input.js').toString()
-  ),
+    module.exports.render_input = eval(
+      fs.readFileSync(harbor_dir + '/ssh/render_input.js').toString()
+    );
+    module.exports.render_work_preview = eval(
+      fs.readFileSync(harbor_dir + '/ssh/render_work_preview.js').toString()
+    );
+    module.exports.update = eval(
+      fs.readFileSync(harbor_dir + '/ssh/update.js').toString()
+    );
+    module.exports.work = eval(
+      fs.readFileSync(harbor_dir + '/ssh/work.js').toString()
+    );
+  },
 
-  render_work_preview: eval(
-    fs.readFileSync(harbor_dir + '/ssh/render_work_preview.js').toString()
-  ),
-
-  register: eval(
-    fs.readFileSync(harbor_dir + '/ssh/register.js').toString()
-  ),
-
-  update: eval(
-    fs.readFileSync(harbor_dir + '/ssh/update.js').toString()
-  ),
-
-  work: eval(
-    fs.readFileSync(harbor_dir + '/ssh/work.js').toString()
-  )
-
+  register: (lanes, users, harbors, shipments) => {
+    Shipments = shipments;
+    return { name, pkgs };
+  },
 };
--- ssh/register.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function register (lanes, users, harbors, shipments) {
-  Shipments = shipments;
-
-  return NAME;
-};
